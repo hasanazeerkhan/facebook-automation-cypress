ec1beb0f8c6d4258a1b918853d2af2a6e1f5bfa8
Hasan <hasanazeerkhan@gmail.com>
2024-10-18 12:09:25 +0530

    feat: added function

--- cypress/e2e/facebookLogin.cy.js
+++ cypress/e2e/facebookLogin.cy.js
@@ -1,39 +1,8 @@
-import { account } from "../support/credentials";
-import { elements } from "../support/webElements";
+import { login } from "../support/function";
 
 describe("Facebook", function () {
   it("Create new account", function () {
-    cy.visit("https://www.facebook.com/");
-    cy.get(".fcb > a").click();
-    cy.get(elements.homePage.createNewAccount).click();
-    cy.get(elements.signUp.firstName).type(account.firstName);
-    cy.get(elements.signUp.lastName).type(account.lastName);
-    cy.get(elements.signUp.monthInDob).type(account.monthInDob);
-    cy.get(elements.signUp.dayInDob).select(account.dayInDob);
-    cy.get(elements.signUp.yearInDob).select(account.yearInDob);
-    cy.get(elements.signUp.mobileNumber).type(account.mobileNumber);
-    cy.get(elements.signUp.password).type(account.password);
-    {
-      var selectableGender = account.gender;
-      var j = 1;
-      switch (selectableGender) {
-        case "Female":
-          j = 1;
-          break;
-
-        case "Male":
-          j = 2;
-          break;
-        case "Custom":
-          j = -1;
-          break;
-      }
-    }
-
-    cy.get(
-      `span[data-name="gender_wrapper"] span label input[value="${j}"]`
-    ).click();
-    cy.get(elements.signUp.signUp).click();
+    login();
   });
   it("Sign up", function () {});
 });
